234a80c3516afd85e846a57105f0915218ec08c6
kmh11 <hoppykev@gmail.com>
2016-09-16 21:23:30 +0000

    First commit

---
+++ chemspell.js
@@ -0,0 +1,20 @@
+var symbols = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Uut', 'Fl', 'Uup', 'Lv', 'Uus', 'Uuo', 'e'];
+var symbolLower;
+
+function _elementSpell(word, spelling) {
+	word = word.toLowerCase();
+	for (i=0; i < symbols.length; i++) {
+		symbolLower = symbols[i].toLowerCase();
+		if (symbolLower === word || symbolLower === word.slice(word.length-symbolLower.length, word.length)) {
+			spelling.push(symbols[i]);
+		}
+		if (symbolLower === word || (symbolLower === word.slice(word.length-symbolLower.length, word.length) && _elementSpell(word.slice(0,word.length-symbolLower.length), spelling))) {
+			return spelling;
+		}
+	}
+}
+
+function elementSpell() {
+	try {alert(_elementSpell(document.getElementById("wordInput").value, []).reverse().join(" "));}
+	catch(e) {alert("That word cannot be spelled with elements.");}
+}
