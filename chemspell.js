6253f448a9f5a655f2feb4c958374ddf91c8b8d3
kmh11 <hoppykev@gmail.com>
2016-09-17 09:43:28 +0000

    Change result formatting Added names of elements Elements displayed in squares

--- chemspell.js
+++ chemspell.js
@@ -1,12 +1,14 @@
 var symbols = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Uut', 'Fl', 'Uup', 'Lv', 'Uus', 'Uuo', 'e'];
+var names = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon', 'Nitrogen', 'Oxygen', 'Fluorine', 'Neon', 'Sodium', 'Magnesium', 'Aluminium', 'Silicon', 'Phosphorus', 'Sulfur', 'Chlorine', 'Argon', 'Potassium', 'Calcium', 'Scandium', 'Titanium', 'Vanadium', 'Chromium', 'Manganese', 'Iron', 'Cobalt', 'Nickel', 'Copper', 'Zinc', 'Gallium', 'Germanium', 'Arsenic', 'Selenium', 'Bromine', 'Krypton', 'Rubidium', 'Strontium', 'Yttrium', 'Zirconium', 'Niobium', 'Molybdenum', 'Technetium', 'Ruthenium', 'Rhodium', 'Palladium', 'Silver', 'Cadmium', 'Indium', 'Tin', 'Antimony', 'Tellurium', 'Iodine', 'Xenon', 'Caesium', 'Barium', 'Lanthanum', 'Cerium', 'Praseodymium', 'Neodymium', 'Promethium', 'Samarium', 'Europium', 'Gadolinium', 'Terbium', 'Dysprosium', 'Holmium', 'Erbium', 'Thulium', 'Ytterbium', 'Lutetium', 'Hafnium', 'Tantalum', 'Tungsten', 'Rhenium', 'Osmium', 'Iridium', 'Platinum', 'Gold', 'Mercury', 'Thallium', 'Lead', 'Bismuth', 'Polonium', 'Astatine', 'Radon', 'Francium', 'Radium', 'Actinium', 'Thorium', 'Protactinium', 'Uranium', 'Neptunium', 'Plutonium', 'Americium', 'Curium', 'Berkelium', 'Californium', 'Einsteinium', 'Fermium', 'Mendelevium', 'Nobelium', 'Lawrencium', 'Rutherfordium', 'Dubnium', 'Seaborgium', 'Bohrium', 'Hassium', 'Meitnerium', 'Darmstadtium', 'Roentgenium', 'Copernicium', 'Ununtrium', 'Flerovium', 'Ununpentium', 'Livermorium', 'Ununseptium', 'Ununoctium', 'Electron'];
 var symbolLower;
+var table;
 
 function _elementSpell(word, spelling) {
 	word = word.toLowerCase();
 	for (i=0; i < symbols.length; i++) {
 		symbolLower = symbols[i].toLowerCase();
 		if (symbolLower === word || symbolLower === word.slice(word.length-symbolLower.length, word.length)) {
-			spelling.push(symbols[i]);
+			spelling.push(i);
 		}
 		if (symbolLower === word || (symbolLower === word.slice(word.length-symbolLower.length, word.length) && _elementSpell(word.slice(0,word.length-symbolLower.length), spelling))) {
 			return spelling;
@@ -15,6 +17,14 @@ function _elementSpell(word, spelling) {
 }
 
 function elementSpell() {
-	try {document.getElementById("answer").innerHTML = "<b>"+_elementSpell(document.getElementById("wordInput").value, []).reverse().join(" ")+"</b>";}
-	catch(e) {document.getElementById("answer").innerHTML = "<b>"+"That word cannot be spelled with elements."+"</b>";}
+	var spelling = _elementSpell(document.getElementById("wordInput").value, []).reverse();
+	table = "<table><tr>";
+	for (i=0; i < spelling.length; i++) {
+		table = table+"<td class=\"symbol\">"+symbols[spelling[i]]+"</td>";
+	}
+	table = table+"</tr><tr>"
+	for (i=0; i < spelling.length; i++) {
+		table = table+"<td class=\"name\">"+names[spelling[i]]+"</td>";
+	}
+	document.getElementById("table").innerHTML = table;
 }
